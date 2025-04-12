a0ea437e399955abe9c283c960b2d6f91be2570b
YoonHeeJun <unijun0109@gmail.com>
2025-04-12 18:56:36 +0900

    :tada:Start: 새 프로젝트 생성

---
+++ src/main/java/com/likelion/sbstudy/SbStudyApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.sbstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbStudyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/likelion/sbstudy/SbStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.sbstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
